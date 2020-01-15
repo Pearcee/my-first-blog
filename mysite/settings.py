f2120bbd41a20d91cb669e8880319cd70ec9fa14
steve <sjpearce@.com>
2020-01-15 22:20:48 +0000

    My app, 3rd commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-uk'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/London'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
